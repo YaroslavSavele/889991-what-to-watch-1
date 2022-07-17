fff8b409937348cf51382505838885e387e2d0cd
YaroslavSavele <asis174@mail.ru>
2022-07-17 15:23:46 +0500

    Моделируем модели

---
+++ app/Models/DirectorFilm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DirectorFilm extends Model
+{
+    use HasFactory;
+
+    protected $table = 'director_film';
+}
